b0f4f313d83ae2e083dfdbfc3b63829acefc2599
Arun Mathew <amathew5@mail.yu.edu>
2023-12-09 22:13:22 -0500

    eda on location

--- src/book_review/data_summary.py
+++ src/book_review/data_summary.py
@@ -18,13 +18,15 @@ def __init__(self):
     pass
 
 
-def cleaning(df):
+def cleaning(df, review, user):
     # Delete the last 3 columns
     df = df.iloc[:, :-3]
     cleaned_df = df.dropna(subset=["Year-Of-Publication"])
     cleaned_df["Year-Of-Publication"] = pd.to_numeric(
         cleaned_df["Year-Of-Publication"], errors="coerce"
     )
+    cleaned_df.insert(5, "Book-Ratings", review["Book-Rating"])
+    cleaned_df.insert(6, "Location", user["Location"])
     return cleaned_df
 
 
